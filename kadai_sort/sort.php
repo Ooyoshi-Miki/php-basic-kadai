ffb0ecc9db11c55e334b70024c3a588239c8837f
Ooyoshi-Miki <149459796+Ooyoshi-Miki@users.noreply.github.com>
2024-01-15 21:40:56 +0900

    kadai_sortディレクトリのsort.phpを改訂した

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,29 +9,34 @@
   <body>
     <p>
       <?php
-        function sort_2way($array, $order) {
-          if ($order === "asc") {
+        function sort_2way($array, $order){
+          // もし引数($order)がtrueだったら昇順にソートする。
+          if ($order) {
+            echo "昇順にソートします。<br>";
             sort($array);
-          } elseif ($order === "desc") {
+          } else {
+            // それ以外だったら降順にソートする。
+            echo "降順にソートします。<br>";
             rsort($array);
           }
-            return $array;
+
+          // 配列の要素を1つずつ出力する。
+          foreach ($array as $num) {
+            echo $num . '<br>';
+          }
         }
-  
-        $nums = array(15, 4, 18, 23, 10);
-        $nums = sort_2way($nums, "asc");
-        echo "昇順にソートします。<br>";
-        foreach ($nums as $num) {
-          echo "$num  <br>";
-        }
-  
-        $nums = sort_2way($nums, "desc");
-        echo "降順にソートします。<br>";
-        foreach ($nums as $num) {
-          echo "$num  <br>";
-        }
+
+        $nums = [15, 4, 18, 23, 10];
+
+        // 昇順でソートして出力
+        sort_2way($nums, true);
+
+        // 改行を入れて見やすくする
+        echo "<br>";
+
+        // 降順でソートして出力
+        sort_2way($nums, false);
       ?>
     </p>
   </body>
-
 </html>
